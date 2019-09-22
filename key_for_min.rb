a6cab4577fcb9700388709c30ddd9b4be5db018d
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-22 21:11:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,11 @@
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
+  
+  # won't iterate thru .each method if hash is empty
+  # (logic never entered the each loop because the hash is empty)
+  
   name_hash.each do |key, value| 
-    #won't iterate thru .each method if hash is empty
-    # (logic never entered the each loop because the hash is empty)
     if min_value == nil
       min_value = value
       min_key = key
