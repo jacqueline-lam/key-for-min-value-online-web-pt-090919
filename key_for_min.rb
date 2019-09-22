cbb24026d5369bac765df40c989f1889566f7a53
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-22 21:07:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
-  name_hash.each do |key, value|
+  name_hash.each do |key, value| #won't iterate thru each method if hash is empty
     if min_value == nil
       min_value = value
       min_key = key
